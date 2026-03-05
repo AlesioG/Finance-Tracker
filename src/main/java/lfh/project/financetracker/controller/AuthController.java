131eaa6db685677d5b183e29bc1660092e5d7441
Alesio Gega <agega19@epoka.edu.al>
2026-03-05 19:13:17 +0100

    Add initial entity, controller, and service classes

---
+++ src/main/java/lfh/project/financetracker/controller/AuthController.java
@@ -0,0 +1,7 @@
+package lfh.project.financetracker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
---
+++ src/main/java/lfh/project/financetracker/service/UserService.java
@@ -0,0 +1,7 @@
+package lfh.project.financetracker.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
